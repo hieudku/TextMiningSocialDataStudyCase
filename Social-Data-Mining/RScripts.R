eb15a9cf50a9fa9c5317f50b49ef1dc2f9189ca2
Huba <hieu.dcu@gmail.com>
2024-03-26 15:12:53 +1300

    Install and import libraries

--- Social-Data-Mining/RScripts.R
+++ Social-Data-Mining/RScripts.R
@@ -1,1 +1,13 @@
 getwd()
+
+# Install packages into R Studio
+install.packages("SnowballC")
+install.packages("tm")
+install.packages("syuzhet")
+install.packages("wordcloud")
+
+# Load libraries into R Studio
+library(SnowballC)
+library(tm)
+library(syuzhet)
+library(wordcloud)
