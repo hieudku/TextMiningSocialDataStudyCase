614c586d10e4b35edbc640068ed5372c20eb0f94
Huba <hieu.dcu@gmail.com>
2024-03-26 21:21:43 +1300

    Applied regular expression to properly clean tweets

--- Social-Data-Mining/RScripts.R
+++ Social-Data-Mining/RScripts.R
@@ -1,17 +1,20 @@
 getwd()
 dir_path <- "C:\\Users\\hieu.cu_it.weltec\\OneDrive - Whitireia and WelTec\\Social-Data\\Assignment1\\DS6501-Assignment1-TextMiningSocialData\\Social-Data-Mining"
 setwd(dir_path)
-# Install packages into R Studio
-install.packages("SnowballC")
-install.packages("tm")
-install.packages("syuzhet")
-install.packages("wordcloud")
 
 # Load libraries into R Studio
 library(SnowballC)
 library(tm)
 library(syuzhet)
 library(wordcloud)
+
+
+# Install packages into R Studio
+install.packages("SnowballC")
+install.packages("tm")
+install.packages("syuzhet")
+install.packages("wordcloud")
+
 
 #############################################################
 # Create Corpus from text files contained in Political folder
@@ -92,6 +95,29 @@ tweets_Labour.df2 <- tweets_Labour.df$text
 head(tweets_Green.df2)
 head(tweets_National.df2)
 head(tweets_Labour.df2)
+
+
+# Define the clean_tweet function
+clean_tweet <- function(tweet) {
+  # Convert text to UTF-8 encoding
+  tweet <- iconv(tweet, "UTF-8", "UTF-8", sub = "byte")
+  # Remove URLs
+  tweet <- gsub("https?://\\S+|www\\.[^\\s]+", "", tweet)
+  # Remove special characters
+  tweet <- gsub("[^[:alnum:]' ]", "", tweet)
+  return(tweet)
+}
+
+# Now you can use clean_tweet to clean your tweets
+tweets_Green.df$text <- sapply(tweets_Green.df$text, clean_tweet)
+tweets_National.df$text <- sapply(tweets_National.df$text, clean_tweet)
+tweets_Labour.df$text <- sapply(tweets_Labour.df$text, clean_tweet)
+
+# Display cleaned tweets
+head(tweets_Green.df$text)
+head(tweets_National.df$text)
+head(tweets_Labour.df$text)
+
 # Use a 'find and replace' function to remove garbage from tweets
 tweets_Green.df2 <- gsub("http.*","",tweets_Green.df2)
 tweets_National.df2 <- gsub("http.*","",tweets_National.df2)
