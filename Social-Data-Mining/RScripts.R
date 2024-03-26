ac14a608b06193f07acab7b866fb33e825fb138b
Huba <hieu.dcu@gmail.com>
2024-03-26 16:01:54 +1300

    blablablah

--- Social-Data-Mining/RScripts.R
+++ Social-Data-Mining/RScripts.R
@@ -12,6 +12,63 @@ library(tm)
 library(syuzhet)
 library(wordcloud)
 
+#############################################################
+# Create Corpus from text files contained in Political folder
+docs <- Corpus(DirSource("Political Parties"))
+
+# View summary of the corpus
+summary(docs)
+
+# View the structure of the corpus
+str(docs)
+
+# Get a list of documents in the corpus
+names(docs)
+
+##########################
+toSpace <- content_transformer(function(x, pattern) {return (gsub(pattern, " ", x))})
+remvPattern <- content_transformer(function(x, pattern) {return (gsub(pattern, "", x))})
+
+# Now we can use this content transformer to eliminate colons and hyphens 
+docs <- tm_map(docs, toSpace, "-")
+docs <- tm_map(docs, remvPattern, "http\\S+")
+docs[[1]]$content
+
+
+###############################
+
+# Remove punctuation - replace punctuation marks with a space
+docs <- tm_map(docs, removePunctuation)
+
+#Transform to lower case (need to wrap in content_transformer)
+docs <- tm_map(docs,content_transformer(tolower))
+
+#Strip digits (std transformation - no need for content_transformer)
+docs <- tm_map(docs, removeNumbers)
+
+#remove stopwords using the standard list in tm
+docs <- tm_map(docs, removeWords, stopwords("english"))
+
+#Strip whitespace 
+docs <- tm_map(docs, stripWhitespace)
+
+#######################
+#Info about the docs
+# View summary of the corpus
+summary(docs)
+
+# View the structure of the corpus
+str(docs)
+
+# Get a list of documents in the corpus
+names(docs)
+
+# Access the content of a specific document
+docs[[1]]$content
+
+#######################
+
+############################################################
 # Import tweets
 tweets_Green.df <- read.csv("./Political Parties/NZGreens_tweets.csv")
 tweets_National.df <- read.csv("./Political Parties/NZNationalParty_tweets.csv")
@@ -31,3 +88,37 @@ tweets_Green.df2 <- tweets_Green.df$text
 tweets_National.df2 <- tweets_National.df$text
 tweets_Labour.df2 <- tweets_Labour.df$text
 
+head(tweets_Green.df2)
+head(tweets_National.df2)
+head(tweets_Labour.df2)
+# Use a 'find and replace' function to remove garbage from tweets
+
+tweets_Green.df2 <- gsub("http.*","",tweets_Green.df2)
+tweets_National.df2 <- gsub("http.*","",tweets_National.df2)
+tweets_Labour.df2 <- gsub("http.*","",tweets_Labour.df2)
+# The '.* means it will remove all text to the right of the pattern
+# found - http in the example above. This would clear the whole tweet
+# if it begin with this pattern, so be careful.
+
+tweets_Green.df2 <- gsub("https.*","",tweets_Green.df2)
+tweets_National.df2 <- gsub("https.*","",tweets_National.df2)
+tweets_Labour.df2 <- gsub("https.*","",tweets_Labour.df2)
+
+# Remove everything shown between the square brackets - very useful
+# Avoids striping the whole tweet, unlike '.*' option
+tweets_Green.df2 <- gsub("[\\]+","",tweets_Green.df2)
+tweets_National.df2 <- gsub("[\\]+","",tweets_National.df2)
+tweets_Labour.df2 <- gsub("[\\]+","",tweets_Labour.df2)
+
+tweets.df2 <- gsub("#.*","",tweets.df2)
+# Display clean text
+#head(tweets.df2)
+
+tweets.df2 <- gsub("@.*","",tweets.df2) # Donald tweets
+tweets.df2 <- gsub("…","",tweets.df2)
+#tweets.df2 <- gsub("@","",tweets.df2) # Airline tweets
+
+# tweets.df2 <- gsub(".","",tweets.df2)
+
+# Display clean text
+head(tweets.df2)
