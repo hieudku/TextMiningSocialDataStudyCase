eb6712b3d9db780e330a4a9037933b42a8b220ea
Huba <hieu.dcu@gmail.com>
2024-03-27 22:59:03 +1300

    Updated RScript.R
    Restructured code for easier readability. Divided into 3 sections for each file for analysis

--- Social-Data-Mining/RScripts.R
+++ Social-Data-Mining/RScripts.R
@@ -61,100 +61,201 @@ head(tweets_Green.df2)
 head(tweets_National.df2)
 head(tweets_Labour.df2)
 
-####################### Sentiment Analysis ###########################
+####################### Sentiment Analysis ############################
 
-#### Green Party ####
+
+####################### Labour Party ##################################
 
 # Convert data frame into a vector before performing sentiment analysis
-word_Green.df <- as.vector(tweets_Green.df2)
-word_National.df <- as.vector(tweets_National.df2)
 word_Labour.df <- as.vector(tweets_Labour.df2)
 
 # Perform sentiment analysis to score tweets on emotion
-emotion_Green.df <- get_nrc_sentiment(word_Green.df)
-emotion_National.df <- get_nrc_sentiment(word_National.df)
 emotion_Labour.df <- get_nrc_sentiment(word_Labour.df)
 
 # Combine tweets to sentiment scores
-emotion_Green <- cbind(tweets_Green.df2, emotion_Green.df) 
-emotion_National <- cbind(tweets_National.df2, emotion_National.df)
 emotion_Labour <- cbind(tweets_Labour.df2, emotion_Labour.df)
-
-head(emotion_Green)
-head(emotion_National)
 head(emotion_Labour)
 
 # Score tweets based on positive and negative sentiment
-sent.value_Green <- get_sentiment(word_Green.df)
-sent.value_National <- get_sentiment(word_National.df)
 sent.value_Labour <- get_sentiment(word_Labour.df)
 
 # Filter based on positive, negative and neutral tweets
-positive.tweets_Green <- word_Green.df[sent.value_Green > 0]
-negative.tweets_Green <- word_Green.df[sent.value_Green < 0]
-neutral.tweets_Green <- word_Green.df[sent.value_Green == 0]
-
-positive.tweets_National <- word_National.df[sent.value_National > 0]
-negative.tweets_National <- word_National.df[sent.value_National < 0]
-neutral.tweets_National <- word_National.df[sent.value_National == 0]
-
 positive.tweets_Labour <- word_Labour.df[sent.value_Labour > 0]
 negative.tweets_Labour <- word_Labour.df[sent.value_Labour < 0]
 neutral.tweets_Labour <- word_Labour.df[sent.value_Labour == 0]
 
 # Count number of positive, negative and neutral tweets
-pos_Green <- length(positive.tweets_Green)
-neut_Green <- length(neutral.tweets_Green)
-neg_Green <- length(negative.tweets_Green)
-
-pos_National <- length(positive.tweets_National)
-neut_National <- length(neutral.tweets_National)
-neg_National <- length(negative.tweets_National)
-
 pos_Labour <- length(positive.tweets_Labour)
 neut_Labour <- length(neutral.tweets_Labour)
 neg_Labour <- length(negative.tweets_Labour)
 
 # Using the values in pos, neut and neg, we can plot a pie chart
 # First combine the values and assign to a vector
-x <- c(pos_Green, neut_Green, neg_Green)
-y <- c(pos_National, neut_National, neg_National)
 z <- c(pos_Labour, neut_Labour, neg_Labour)
 
 # Now define the labels to be used in pie chart 
-labels_Green <- c("Positive", "Negative", "Neutral")
-labels_National <- c("Positive", "Negative", "Neutral")
 labels_Labour <- c("Positive", "Negative", "Neutral")
 
 # Finally, plot the chart
-pie(x, labels_Green, main = "Green Party's Sentiment Analysis", col = rainbow(length(x)))
-pie(y, labels_National, main = "Nationtal Party's Sentiment Analysis", col = rainbow(length(y)))
 pie(z, labels_Labour, main = "Labour Party's Sentiment Analysis", col = rainbow(length(z)))
 
 # Select the most positive sentiment (highest sent.value)
-most.positive_Green <- word_Green.df[sent.value_Green == max(sent.value_Green)]
-most.positive_National <- word_National.df[sent.value_National == max(sent.value_National)]
 most.positive_Labour <- word_Labour.df[sent.value_Labour == max(sent.value_Labour)]
 
 # Display tweet with most positive sentiment
-most.positive_Green
-most.positive_National
 most.positive_Labour
 
 # Select the most negative sentiment (lowest sent.value)
-most.negative_Green <- word_Green.df[sent.value_Green <= min(sent.value_Green)]
-most.negative_National <- word_National.df[sent.value_National <= min(sent.value_National)]
 most.negative_Labour <- word_Labour.df[sent.value_Labour <= min(sent.value_Labour)]
 
 # Display tweet with most negative sentiment
-most.negative_Green
-most.negative_National
 most.negative_Labour
 
 # Create a corpus (collection of words) from our data frame of cleaned tweets
-tweet_corpus_Green <- Corpus(VectorSource(word_Green.df))
-tweet_corpus_National <- Corpus(VectorSource(word_National.df))
 tweet_corpus_Labour <- Corpus(VectorSource(word_Labour.df))
+
+# create term document matrix applying some transformations
+tdm_Labour <- TermDocumentMatrix(tweet_corpus_Labour,
+                                 control = list(removeNumbers = TRUE, wordLengths=c(5, 15),
+                                                stopwords = c("Labour", "Party", "party", "government", "Government", "tweet", stopwords("english")),
+                                                removeNumbers = TRUE, tolower = TRUE))
+
+# define tdm as matrix so we can calculate word frequencies
+tdm.matrix_Labour <- as.matrix(tdm_Labour)
+
+# get word counts in decreasing order
+word_freqs_Labour <- sort(rowSums(tdm.matrix_Labour), decreasing=TRUE) 
+
+# create a data frame with words and their frequencies
+dm_Labour <- data.frame(word=names(word_freqs_Labour), freq=word_freqs_Labour)
+
+# plot wordcloud with maximum of 50 words, excluding stopwords
+wordcloud(dm_Labour$word, dm_Labour$freq, min.freq = 10, max.words = 50,
+          random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+
+#######################################################################
+####################### National Party ################################
+
+# Convert data frame into a vector before performing sentiment analysis
+word_National.df <- as.vector(tweets_National.df2)
+
+# Perform sentiment analysis to score tweets on emotion
+emotion_National.df <- get_nrc_sentiment(word_National.df)
+
+# Combine tweets to sentiment scores
+emotion_National <- cbind(tweets_National.df2, emotion_National.df)
+head(emotion_National)
+
+# Score tweets based on positive and negative sentiment
+sent.value_National <- get_sentiment(word_National.df)
+
+# Filter based on positive, negative and neutral tweets
+positive.tweets_National <- word_National.df[sent.value_National > 0]
+negative.tweets_National <- word_National.df[sent.value_National < 0]
+neutral.tweets_National <- word_National.df[sent.value_National == 0]
+
+# Count number of positive, negative and neutral tweets
+pos_National <- length(positive.tweets_National)
+neut_National <- length(neutral.tweets_National)
+neg_National <- length(negative.tweets_National)
+
+# Using the values in pos, neut and neg, we can plot a pie chart
+# First combine the values and assign to a vector
+y <- c(pos_National, neut_National, neg_National)
+
+# Now define the labels to be used in pie chart 
+labels_National <- c("Positive", "Negative", "Neutral")
+
+# Finally, plot the chart
+pie(y, labels_National, main = "Nationtal Party's Sentiment Analysis", col = rainbow(length(y)))
+
+# Select the most positive sentiment (highest sent.value)
+most.positive_National <- word_National.df[sent.value_National == max(sent.value_National)]
+
+# Display tweet with most positive sentiment
+most.positive_National
+
+# Select the most negative sentiment (lowest sent.value)
+most.negative_National <- word_National.df[sent.value_National <= min(sent.value_National)]
+
+# Display tweet with most negative sentiment
+most.negative_National
+
+# Create a corpus (collection of words) from our data frame of cleaned tweets
+tweet_corpus_National <- Corpus(VectorSource(word_National.df))
+
+# create term document matrix applying some transformations
+
+tdm_National <- TermDocumentMatrix(tweet_corpus_National,
+                                   control = list(removeNumbers = TRUE, wordLengths=c(5, 15),
+                                                  stopwords = c("National", "Party", "party", "government", "Government", "tweet", stopwords("english")),
+                                                  removeNumbers = TRUE, tolower = TRUE))
+
+# define tdm as matrix so we can calculate word frequencies
+tdm.matrix_National <- as.matrix(tdm_National)
+
+# get word counts in decreasing order
+word_freqs_National <- sort(rowSums(tdm.matrix_National), decreasing=TRUE) 
+
+# create a data frame with words and their frequencies
+dm_National <- data.frame(word=names(word_freqs_National), freq=word_freqs_National)
+
+# plot wordcloud with maximum of 50 words, excluding stopwords
+wordcloud(dm_National$word, dm_National$freq, min.freq = 2, max.words = 50,
+          random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+
+
+
+#######################################################################
+####################### Green Party ###################################
+
+# Convert data frame into a vector before performing sentiment analysis
+word_Green.df <- as.vector(tweets_Green.df2)
+
+# Perform sentiment analysis to score tweets on emotion
+emotion_Green.df <- get_nrc_sentiment(word_Green.df)
+
+# Combine tweets to sentiment scores
+emotion_Green <- cbind(tweets_Green.df2, emotion_Green.df) 
+head(emotion_Green)
+
+# Score tweets based on positive and negative sentiment
+sent.value_Green <- get_sentiment(word_Green.df)
+
+# Filter based on positive, negative and neutral tweets
+positive.tweets_Green <- word_Green.df[sent.value_Green > 0]
+negative.tweets_Green <- word_Green.df[sent.value_Green < 0]
+neutral.tweets_Green <- word_Green.df[sent.value_Green == 0]
+
+# Count number of positive, negative and neutral tweets
+pos_Green <- length(positive.tweets_Green)
+neut_Green <- length(neutral.tweets_Green)
+neg_Green <- length(negative.tweets_Green)
+
+# Using the values in pos, neut and neg, we can plot a pie chart
+# First combine the values and assign to a vector
+x <- c(pos_Green, neut_Green, neg_Green)
+
+# Now define the labels to be used in pie chart 
+labels_Green <- c("Positive", "Negative", "Neutral")
+
+# Finally, plot the chart
+pie(x, labels_Green, main = "Green Party's Sentiment Analysis", col = rainbow(length(x)))
+
+# Select the most positive sentiment (highest sent.value)
+most.positive_Green <- word_Green.df[sent.value_Green == max(sent.value_Green)]
+
+# Display tweet with most positive sentiment
+most.positive_Green
+
+# Select the most negative sentiment (lowest sent.value)
+most.negative_Green <- word_Green.df[sent.value_Green <= min(sent.value_Green)]
+
+# Display tweet with most negative sentiment
+most.negative_Green
+
+# Create a corpus (collection of words) from our data frame of cleaned tweets
+tweet_corpus_Green <- Corpus(VectorSource(word_Green.df))
 
 # create term document matrix applying some transformations
 tdm_Green <- TermDocumentMatrix(tweet_corpus_Green,
@@ -162,39 +263,17 @@ tdm_Green <- TermDocumentMatrix(tweet_corpus_Green,
                                          stopwords = c("Green", "green", "greens", "Party", "party", "government", "Government", "tweet", stopwords("english")),
                                          removeNumbers = TRUE, tolower = TRUE))
 
-tdm_National <- TermDocumentMatrix(tweet_corpus_National,
-                                control = list(removeNumbers = TRUE, wordLengths=c(5, 15),
-                                               stopwords = c("National", "Party", "party", "government", "Government", "tweet", stopwords("english")),
-                                               removeNumbers = TRUE, tolower = TRUE))
-
-tdm_Labour <- TermDocumentMatrix(tweet_corpus_Labour,
-                                control = list(removeNumbers = TRUE, wordLengths=c(5, 15),
-                                               stopwords = c("Labour", "Party", "party", "government", "Government", "tweet", stopwords("english")),
-                                               removeNumbers = TRUE, tolower = TRUE))
-
 # define tdm as matrix so we can calculate word frequencies
 tdm.matrix_Green <- as.matrix(tdm_Green)
-tdm.matrix_National <- as.matrix(tdm_National)
-tdm.matrix_Labour <- as.matrix(tdm_Labour)
 
 # get word counts in decreasing order
 word_freqs_Green <- sort(rowSums(tdm.matrix_Green), decreasing=TRUE) 
-word_freqs_National <- sort(rowSums(tdm.matrix_National), decreasing=TRUE) 
-word_freqs_Labour <- sort(rowSums(tdm.matrix_Labour), decreasing=TRUE) 
 
 # create a data frame with words and their frequencies
 dm_Green <- data.frame(word=names(word_freqs_Green), freq=word_freqs_Green)
-dm_National <- data.frame(word=names(word_freqs_National), freq=word_freqs_National)
-dm_Labour <- data.frame(word=names(word_freqs_Labour), freq=word_freqs_Labour)
 
 # plot wordcloud with maximum of 50 words, excluding stopwords
 wordcloud(dm_Green$word, dm_Green$freq, min.freq = 10, max.words = 50,
           random.order=FALSE, colors=brewer.pal(8, "Dark2"))
 
-wordcloud(dm_National$word, dm_National$freq, min.freq = 10, max.words = 50,
-          random.order=FALSE, colors=brewer.pal(8, "Dark2"))
-
-wordcloud(dm_Labour$word, dm_Labour$freq, min.freq = 10, max.words = 50,
-          random.order=FALSE, colors=brewer.pal(8, "Dark2"))
-
-#####################
+###########################################################################
